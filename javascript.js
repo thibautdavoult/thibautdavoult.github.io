546ef8b6df41f8898d0dc777a5411b9a29fcb677
Thibaut Davoult <thibaut@teleportd.com>
2015-06-27 01:58:43 +0200

    switchtext launch

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $("#film_rating").html(data.results.collection1[0].film_rating + "/10");
         $("#film_director").text(data.results.collection1[0].film_director.text);
         $("#opinion").text(opinion(data.results.collection1[0].film_rating));
-      };
+      }
 
        function opinion(rating) {
         if (rating == 10) {
@@ -32,27 +32,27 @@ $(document).ready(function() {
     }
 
 
-      $.ajax({
-        url: "http://www.kimonolabs.com/api/1vvwp5au?apikey=a51813d8c7299c1e2506b8b8654c2bd4&callback=imdb",
-        jsonp: "imdb",
-        crossDomain: true,
-        dataType: "jsonp"
-      });
+  $.ajax({
+    url: "http://www.kimonolabs.com/api/1vvwp5au?apikey=a51813d8c7299c1e2506b8b8654c2bd4&callback=imdb",
+    jsonp: "imdb",
+    crossDomain: true,
+    dataType: "jsonp"
+  });
 
-      function lastfm(data) {
-        $("#album").html("<a href='" + data.results.collection1[0].Album.href + "' target='_blank'>" + data.results.collection1[0].Album.text + "</a>");
-        $("#band").text(data.results.collection1[0].Band.text);
+  function lastfm(data) {
+    $("#album").html("<a href='" + data.results.collection1[0].Album.href + "' target='_blank'>" + data.results.collection1[0].Album.text + "</a>");
+    $("#band").text(data.results.collection1[0].Band.text);
 
-      }
+  }
 
-      $.ajax({
-        url: "https://www.kimonolabs.com/api/aocumgg0?apikey=a51813d8c7299c1e2506b8b8654c2bd4&callback=lastfm",
-        jsonp: "lastfm",
-        crossDomain :true,
-        dataType: "jsonp"
-      });
+  $.ajax({
+    url: "https://www.kimonolabs.com/api/aocumgg0?apikey=a51813d8c7299c1e2506b8b8654c2bd4&callback=lastfm",
+    jsonp: "lastfm",
+    crossDomain :true,
+    dataType: "jsonp"
+  });
 
-    $(window).load(function() {
-        $('body').css({visibility: 'visible'});
+  $(window).load(function() {
+    $('body').css({visibility: 'visible'});
     });
 });
