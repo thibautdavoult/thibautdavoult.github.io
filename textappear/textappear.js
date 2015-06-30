585ec542b1af152d3342b537100d6f0a30c8f242
Thibaut Davoult <thibaut@teleportd.com>
2015-06-30 23:30:07 +0200

    textappear

--- textappear/textappear.js
+++ textappear/textappear.js
@@ -1,9 +1,26 @@
-
 $("document").ready(function() {
-
-  $("span").click(function() {
-    $(this).next().removeClass("hidden");
-
+  $(".clickappear").click(function() {
+    $(".choice1").removeClass("hidden");
   });
-
+  $(".clickappear2").click(function() {
+    $(".choice2").removeClass("hidden");
+  });
+  $(".clickappear3").click(function() {
+    $(".choice3").removeClass("hidden");
+  });
+  $(".clickappear12").click(function() {
+    $(".choice12").removeClass("hidden");
+  });
+  $(".clickappear13").click(function() {
+    $(".choice13").removeClass("hidden");
+  });
+  $(".clickappear31").click(function() {
+    $(".choice31").removeClass("hidden");
+  });
+  $(".clickappear32").click(function() {
+    $(".choice32").removeClass("hidden");
+  });
+  $(".easteregg").click(function() {
+    $(".choiceegg").removeClass("hidden");
+  });
 });
