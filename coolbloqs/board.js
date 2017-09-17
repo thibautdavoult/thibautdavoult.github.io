0514715d22c58822ebc250be2f2551f822ecbfdc
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-09-17 17:37:34 +0200

    some stuff and deleted list folder

--- coolbloqs/board.js
+++ coolbloqs/board.js
@@ -1,3 +1,13 @@
 //****************************************
 // Game UI and Behavior on Screen goes here
 //****************************************
+
+$(document).ready(function(){
+
+// some jquery stuff goes here
+
+
+
+});
+
+// some other stuff goes here
--- coolbloqs/logic.js
+++ coolbloqs/logic.js
@@ -3,10 +3,11 @@
 //****************************************
 
 function newBoard () {
-  this.board = []; // Board layout (1st iteration is 14x14 tiles)
+  this.board = []; // Board layout (1st iteration is 14x14 tiles, may need a constuctor to let users choose board size)
   this.ended = false; // Checking if game is over
   this.playerTurn = 0; // Turn based 1v1 game. Starts with Player 1 turn, changes from 0 to 1 for Player 2 and back to 0
   this.tiles = ["red", "green", "blue", "purple"]; // Possible tiles colors (1st iteration has 4 fixed colors)
+  this.ownTiles = []; // Set of tiles owned by the player
   this._generateBoard(); // Puts tiles randomly across the board on game initiation
 }
 
@@ -14,3 +15,9 @@ function newBoard () {
 //****************************************
 // Game Logic and Funtions go here
 //****************************************
+
+// 1. Random tile creator
+
+// 2. Generating a board filled with random tiles
+
+// 3. Handling each player's owned tiles (?)
