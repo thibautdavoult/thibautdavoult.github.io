6bca1174381dcb7e77bfc3066310eaeb437c26c1
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-09-17 02:53:33 +0200

    removed unneeded folders

--- bigasstext/th.js
+++
@@ -1,8 +0,0 @@
-$(document).ready ( function () {
-	var url = "https://teamtreehouse.com/thibautdavoult.json";
-		$.getJSON(url, function treehouse(response) {
-			var jsPoints = response.points.JavaScript;
-			console.log(jsPoints);
-			$('#th').html(jsPoints);
-		});
-});
