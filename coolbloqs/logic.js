3c058370a62eb75624eb3bad0eca2c61b53bb3ff
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-09-18 12:30:34 +0200

    random tile color function

--- coolbloqs/logic.js
+++ coolbloqs/logic.js
@@ -2,13 +2,24 @@
 // Definig the board object constructor
 //****************************************
 
-function newBoard () {
-  this.board = []; // Board layout (1st iteration is 14x14 tiles, may need a constuctor to let users choose board size)
+function FloodIt () {
+   // Board layout (1st iteration is 14x14 tiles, may need a constuctor to let users choose board size)
+  this.board = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null]
+  ];
+  this.tiles = ["red", "green", "blue", "purple"]; // Possible tiles colors (1st iteration has 4 fixed colors)
+  // this._generateBoard(); // Puts tiles randomly across the board on game initiation
+/* Later stuff
+
   this.ended = false; // Checking if game is over
   this.playerTurn = 0; // Turn based 1v1 game. Starts with Player 1 turn, changes from 0 to 1 for Player 2 and back to 0
-  this.tiles = ["red", "green", "blue", "purple"]; // Possible tiles colors (1st iteration has 4 fixed colors)
   this.ownTiles = []; // Set of tiles owned by the player
-  this._generateBoard(); // Puts tiles randomly across the board on game initiation
+
+*/
+
 }
 
 
@@ -16,8 +27,24 @@ function newBoard () {
 // Game Logic and Funtions go here
 //****************************************
 
-// 1. Random tile creator
+flood = new FloodIt();
+
+// 1. Picking a random tile color
+
+FloodIt.prototype.randomTileColor = function() {
+  var index = Math.floor(Math.random() * 4);
+  var tileColor = this.tiles[index];
+  console.log(tileColor);
+};
+
+// ****** ACTIVATE TO TEST FUNCTION
+// flood.randomTileColor();
+// ****** END ACTIVATE TO TEST FUNCTION
 
 // 2. Generating a board filled with random tiles
+
+
+
+
 
 // 3. Handling each player's owned tiles (?)
